e32698c0a78c780c401ff711c7776bfe83dae2ef
Ben Clark <benclarkphoto@mac.com>
2022-05-22 23:21:06 -0700

    Add barba page transitions

--- app.js
+++ app.js
@@ -109,9 +109,55 @@ function navToggle(e) {
     }
 }
 
+// Barba page transitions
+barba.init({
+    views: [
+        {
+            namespace: 'home',
+            beforeEnter(){
+                animateSlides();
+            },
+            beforeLeave(){
+                slideScene.destroy();
+                pageScene.destroy();
+                controller.destroy();
+            }
+        },
+        {
+            namespace: 'second'
+        }
+    ],
+    transitions: [
+        {
+            leave({current,next}){
+                let done = this.async();
+                // Animation
+                const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+                tl.fromTo(
+                    current.container, 
+                    1, 
+                    { opacity: 1 }, 
+                    { opacity: 0, onComplete: done }
+                );
+            },
+            enter({current,next}){
+                let done = this.async();
+                // Scroll to the top
+                window.scrollTo(0, 0);
+                // Animation
+                const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+                tl.fromTo(
+                    next.container, 
+                    1, 
+                    { opacity: 0 }, 
+                    { opacity: 1, onComplete: done });
+            }
+        }
+    ]
+})
 //Event Listeners
 burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
 
-animateSlides();
+// animateSlides();
